a80d9376743eb468b7761d7ee0316e051178f744
KotaShogo <>
2022-12-13 11:49:14 +0900

    モーダルを表示するところまで

---
+++ Assets/Scripts/ModalBase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ModalBase : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI titleText;
+
+    [SerializeField]
+    private TextMeshProUGUI messageText;
+
+    [SerializeField]
+    private Button YesButton;
+
+    [SerializeField]
+    private Button NoButton;
+
+    // 処理を委譲します
+    public delegate void ModalEventDelegate();
+
+    public void SetTwoButtonModal(string title,string message,
+        ModalEventDelegate yesEvent,ModalEventDelegate noEvent)
+    {
+        this.gameObject.SetActive(true);
+        titleText.text = title;
+        messageText.text = message;
+        YesButton.onClick.AddListener(()=> yesEvent());
+        NoButton.onClick.AddListener(() => noEvent());
+    }
+
+}
--- Assets/Scripts/RetirePoint.cs
+++ Assets/Scripts/RetirePoint.cs
@@ -15,19 +15,25 @@ public class RetirePoint : MonoBehaviour
         TextModalPrefab = Instantiate(TextModalPrefab);
         TextModalPrefab.SetActive(false);
         int layerNo = LayerMask.NameToLayer("Player");
-        // マスクへの変換（ビットシフト）
-        playerLayar = 1 << layerNo;
+        playerLayar = layerNo;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    { 
         if (collision.gameObject.layer == playerLayar)
         {
+
+            Debug.Log(collision.name);
             TextModalPrefab.SetActive(true);
             var characters = FindObjectsOfType<CharacterBase>();
             foreach (var character in characters) {
                 // キャラクターの移動を不活性にする
                 character.isActive = false;
+                var modal = TextModalPrefab.GetComponent<ModalBase>();
+                modal.SetTwoButtonModal("RetirePoint", "do you want to retire?",
+                    () => { Debug.Log("yes"); },
+                    () => { Debug.Log("No"); });
+
             }
         }
     }
