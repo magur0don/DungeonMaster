0a897c01cbbe75e6dd44ba984ca5efc9bb769af2
KotaShogo <>
2022-12-22 17:46:35 +0900

    フォルダ内の整理

--- Assets/Scripts/Scene/TitleSceneManager.cs
+++ Assets/Scripts/Scene/TitleSceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DungeonTitleManager : MonoBehaviour
+public class TitleSceneManager : MonoBehaviour
 {
     [SerializeField]
     Button startButton;
